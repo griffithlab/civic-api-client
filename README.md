0110bc6efa2f6fdb1e07f591f8133fb8cd9f54cc
Avinash Ramu <trackavinash@gmail.com>
2016-01-19 15:53:13 -0600

    Fix no coordinate case

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # civic-api-client
-Example tools using the CIVIC API
+Example tools using the CIVIC API. Very basic docs on using the API is
+[here](https://civic.genome.wustl.edu/#/api-documentation). The schemas
+can be viewed [here](https://github.com/genome/civic-server/blob/deploy/db/schema.rb)
+
+##Setup
+Install like any other Python package, I like using the Virtualenv setup.
+``` bash
+git clone https://github.com/griffithlab/civic-api-client
+cd civic-api-client
+virtualenv myenv && source myenv/bin/activate
+pip install .
+```
--- civic_api_client/variants_lister.py
+++ civic_api_client/variants_lister.py
@@ -13,7 +13,7 @@ class VariantsLister:
     #URL to the CIVIC API
     civic_url = 'https://civic.genome.wustl.edu/api/'
     #Max number of genes to query
-    max_gene_count = 10
+    max_gene_count = 100000
     #List of genes that were queried for
     genes = []
     #Arguments to this tool
@@ -50,22 +50,22 @@ class VariantsLister:
         "Get the details for a variant given an ID"
         variant_url = self.civic_url + 'variants/' + str(variant_id)
         return requests.get(variant_url).json()
-    def parse_variant_details(self, variant_details):
+    def parse_variant_details(self, variant_id, variant_details):
         "Parse the details of the variants"
-        if self.args.no_coords:
-            if 'coordinates' in variant_details:
-                print   "ID: ", variant_details['id'], \
-                        "Name: ", variant_details['name'], \
-                        "Coordinate1: ", \
-                        variant_details['coordinates']['chromosome'],\
-                        variant_details['coordinates']['start'], \
-                        variant_details['coordinates']['stop'], \
-                        "Coordinate2: ", \
-                        variant_details['coordinates']['chromosome2'],\
-                        variant_details['coordinates']['start2'],\
-                        variant_details['coordinates']['stop2']
-            else:
-                print "coordinates unavailable: ", variant_name, variant_id
+        if 'coordinates' in variant_details:
+            print "ID: ", variant_details['id'], \
+                    "Name: ", variant_details['name'], \
+                    "Coordinate1: ", \
+                    variant_details['coordinates']['chromosome'],\
+                    variant_details['coordinates']['start'], \
+                    variant_details['coordinates']['stop'], \
+                    "Coordinate2: ", \
+                    variant_details['coordinates']['chromosome2'],\
+                    variant_details['coordinates']['start2'],\
+                    variant_details['coordinates']['stop2']
+        elif self.args.no_coords:
+            print "coordinates unavailable: ", \
+                  variant_id
     def main(self):
         "Execution starts here"
         self.args = self.parse_args()
@@ -73,4 +73,4 @@ class VariantsLister:
         variant_ids = self.get_variant_ids()
         for variant_id in variant_ids:
             variant_details = self.get_variant_details(variant_id)
-            self.parse_variant_details(variant_details)
+            self.parse_variant_details(variant_id, variant_details)
