36a30a779eb5ddceb841749ea1e43ed7b71e0c84
Avinash Ramu <trackavinash@gmail.com>
2016-02-02 16:10:50 -0600

    Add evidence-items lister

--- civic_api_client/command_line.py
+++ civic_api_client/command_line.py
@@ -2,12 +2,14 @@ import sys
 
 import civic_api_client
 from variants_lister import VariantsLister
+from evidence_items_lister import EvidenceItemsLister
 
 def usage():
     "Defines usage for the tool"
     print "civic-api-client command options"
     print "Commands:"
     print "\tvariants-list"
+    print "\tevidence-items-list"
 
 def main():
     "Everything starts here"
@@ -15,6 +17,9 @@ def main():
         if sys.argv[1] == "variants-list":
             vl1 = VariantsLister(sys.argv[2:])
             return vl1.main()
+        if sys.argv[1] == "evidence-items-list":
+            eil1 = EvidenceItemsLister(sys.argv[2:])
+            return eil1.main()
     return usage()
 
 if __name__ == '__main__':
---
+++ civic_api_client/evidence_items_lister.py
@@ -0,0 +1,99 @@
+"variants_lister.py - Get variants using the CIVIC API"
+
+import argparse
+from flask import Flask, render_template, url_for
+import json
+import requests
+
+import civic_api_client
+import utils
+from variants_lister import VariantsLister, VariantDetails
+
+class EvidenceItems:
+    variant_id = ""
+    doid = ""
+    variant_civic_url = ""
+
+    def __init__(self, variant_id, doid, variant_civic_url):
+        "Constructor"
+        self.variant_id = variant_id
+        self.doid = doid
+        self.variant_civic_url = variant_civic_url
+
+class EvidenceItemsLister:
+    """Represent the evidence-items in CIVIC"""
+    queried_doids = {}
+    invalid_eis = []
+    def __init__(self, args):
+        "Constructor"
+        self.args = args
+    def parse_args(self):
+        "Parse command-line arguments"
+        parser = argparse.ArgumentParser(description="civic-api-client version {}".format(civic_api_client.__version__),
+            usage = "civic-api-client evidence-items-list",
+            formatter_class = argparse.RawTextHelpFormatter,
+        )
+        parser.add_argument("--doid",
+            action='store_true',
+            help = "Print evidence-items with improper DOID.(not defined"\
+                    "on disease-ontology.org)"
+        )
+        parser.add_argument("--max-gene-count",
+            help = "Maximum number of genes to query from CIVIC [100,000]",
+            type = int,
+            default = 100000
+        )
+        parser.add_argument("--web",
+            action='store_true',
+            help = "Publish evidence-items to a webpage."
+        )
+        args = parser.parse_args(self.args)
+        return args
+
+    def check_evidence_items(self, variant_id, variant_detail, evidence_items):
+        "Check if evidence item is valid"
+        for evidence_item in evidence_items:
+            doid = evidence_item['disease']['doid']
+            if doid not in self.queried_doids:
+                self.queried_doids[doid] = 1
+                url = utils.disease_ontology_api_url() + \
+                        "metadata/DOID:" + str(doid)
+                r = requests.get(url)
+                try:
+                    r.raise_for_status()
+                except requests.exceptions.HTTPError:
+                    ei1 = EvidenceItems(variant_id, doid, \
+                                        VariantDetails.define_civic_url(variant_detail))
+                    self.invalid_eis.append(ei1)
+
+    def display_invalid(self):
+        "Display the invalid DOIDs"
+        if self.args.web:
+            self.display_invalid_web()
+        else:
+            print "Printing invalid DOIDs"
+            for ei1 in self.invalid_eis:
+                print ei1.doid + "\t" + ei1.variant_id + "\t" + ei1.variant_civic_url + "\n"
+
+    def display_invalid_web(self):
+        "Publish to web page"
+        app = Flask("civic_api_client")
+        @app.route("/")
+        def template_test():
+                return render_template('evidence-items.html', \
+                        invalid_eis = self.invalid_eis)
+        app.run(debug=True)
+
+    def main(self):
+        "Execution starts here"
+        self.args = self.parse_args()
+        #Use the variantslister to get all the variants
+        vl1 = VariantsLister(self.args)
+        vl1.get_civic_genes()
+        variant_ids = vl1.get_variant_ids()
+        for variant_id in variant_ids:
+            variant_detail = vl1.get_variant_details(variant_id)
+            if "evidence_items" in variant_detail:
+                evidence_items = variant_detail['evidence_items']
+                self.check_evidence_items(variant_id, variant_detail, evidence_items)
+        self.display_invalid()
