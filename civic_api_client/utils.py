8d19821deb85d0e9cb554374a777d7b4a8cf117c
Avinash Ramu <trackavinash@gmail.com>
2016-02-02 16:10:09 -0600

    Add Utils

---
+++ civic_api_client/utils.py
@@ -0,0 +1,7 @@
+def disease_ontology_api_url():
+    "URL to the disease ontology API"
+    return "http://www.disease-ontology.org/api/"
+
+def civic_api_url():
+    "URL to the CIVIC API"
+    return "https://civic.genome.wustl.edu/api/"
