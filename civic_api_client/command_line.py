be51619968aad91c686985529b65d1dc4be581f2
Avinash Ramu <trackavinash@gmail.com>
2016-02-14 21:55:34 -0600

    Update web-client

--- civic_api_client/command_line.py
+++ civic_api_client/command_line.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template
 import sys
 
 import civic_api_client
@@ -10,6 +11,7 @@ def usage():
     print "Commands:"
     print "\tvariants-list"
     print "\tevidence-items-list"
+    print "\taction-items-web-view"
 
 def main():
     "Everything starts here"
@@ -17,10 +19,37 @@ def main():
         if sys.argv[1] == "variants-list":
             vl1 = VariantsLister(sys.argv[2:])
             return vl1.main()
-        if sys.argv[1] == "evidence-items-list":
+        elif sys.argv[1] == "evidence-items-list":
             eil1 = EvidenceItemsLister(sys.argv[2:])
             return eil1.main()
-    return usage()
+        elif sys.argv[1] == "action-items-web-view":
+            web_view()
+        else:
+            return usage()
+
+def web_view():
+    "Setup the webview"
+    app = Flask("civic_api_client")
+    @app.route("/")
+    def template_home():
+        return render_template('home.html')
+    @app.route("/evidence-items")
+    def evidence_items():
+        eil1 = EvidenceItemsLister(sys.argv[2:])
+        eil1.parse_args()
+        eil1.create_invalid_eis_list()
+        invalid_eis = eil1.get_invalid_eis()
+        return render_template('evidence-items.html', \
+                invalid_eis = invalid_eis)
+    @app.route("/variants")
+    def variants():
+        vl1 = VariantsLister(sys.argv[2:])
+        vl1.parse_args()
+        vl1.create_filtered_variants_list()
+        filtered_variant_details = vl1.get_filtered_variant_details()
+        return render_template('variants.html', \
+                filtered_variant_details= filtered_variant_details)
+    app.run(debug=True)
 
 if __name__ == '__main__':
     main()
