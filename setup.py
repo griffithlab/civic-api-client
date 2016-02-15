47d41b31e24541055e0849f738966f47d26c3b4b
Avinash Ramu <trackavinash@gmail.com>
2016-02-15 15:21:03 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='civic_api_client',
-      version = '0.1.0',
+      version = '0.2.0',
       description = 'Examples and tools for using the CIVIC API',
       install_requires = [
           'requests',
